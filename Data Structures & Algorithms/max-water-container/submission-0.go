ffaaed03e06ada9a9bebbc5150444909c7cb1715
gerardvega <101549941+gerardangelovega@users.noreply.github.com>
2026-06-09 14:48:01 +0800

    Add: max-water-container - submission-0

---
+++ Data Structures & Algorithms/max-water-container/submission-0.go
@@ -0,0 +1,24 @@
+func maxArea(heights []int) int {
+	res := 0
+	n := len(heights)
+
+	for l, r := 0, n-1; l < r; {
+		d := r - l
+		lh, rh := heights[l], heights[r]
+		h := min(lh, rh)
+		res = max(res, h * d)
+
+		if lh < rh {
+			l++
+			continue
+		}
+		if rh < lh {
+			r--
+			continue
+		}
+		l++
+		r--
+	}
+
+	return res
+}
