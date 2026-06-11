a5938817138b38650a051ca78d4996f00e9c0183
gerardvega <101549941+gerardangelovega@users.noreply.github.com>
2026-06-11 12:26:19 +0800

    Add: validate-parentheses - submission-6

---
+++ Data Structures & Algorithms/validate-parentheses/submission-6.go
@@ -0,0 +1,58 @@
+type Stack struct {
+	arr []rune
+}
+func (s *Stack) Push(val rune) {
+	s.arr = append(s.arr, val)
+}
+func (s *Stack) Pop() rune {
+	n := len(s.arr)
+	val := s.arr[n - 1]
+	s.arr = s.arr[:n - 1]
+
+	return val
+}
+func (s *Stack) Peek() rune {
+	n := len(s.arr)
+	val := s.arr[n - 1]
+
+	return val
+}
+func (s *Stack) Length() int {
+	return len(s.arr)
+}
+
+func isValid(s string) bool {
+	if len(s) & 1 == 1 {
+		return false
+	}
+	
+	stack := Stack{
+		arr: make([]rune, 0, len(s)),
+	}
+	pairs := map[rune]rune{
+		'(': ')',
+		'{': '}',
+		'[': ']',
+	}
+
+    for _, r := range s {
+		switch r {
+		case ')', '}', ']':
+			if stack.Length() == 0 {
+				return false
+			}
+			if pairs[stack.Peek()] != r {
+				return false
+			}
+			stack.Pop()
+		default:
+			stack.Push(r)
+		}
+	}
+
+	if stack.Length() != 0 {
+		return false
+	}
+
+	return true
+}
