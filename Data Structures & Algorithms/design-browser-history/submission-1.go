9108a5e488f839867eefbf88da12325bad2f492d
gerardvega <101549941+gerardangelovega@users.noreply.github.com>
2026-06-11 18:43:39 +0800

    Add: design-browser-history - submission-1

---
+++ Data Structures & Algorithms/design-browser-history/submission-1.go
@@ -0,0 +1,69 @@
+type BrowserHistoryNode struct {
+	val   string
+	prev *BrowserHistoryNode 
+	next *BrowserHistoryNode 
+}
+
+type BrowserHistory struct {
+	head *BrowserHistoryNode 
+	curr *BrowserHistoryNode 
+	tail *BrowserHistoryNode 
+	size  int
+}
+
+
+func Constructor(homepage string) BrowserHistory {
+    node := &BrowserHistoryNode{
+		val:  homepage,
+		next: nil,
+		prev: nil,
+	}
+	return BrowserHistory{
+		head: node,
+		curr: node,
+		tail: node,
+	}
+}
+
+
+func (this *BrowserHistory) Visit(url string)  {
+    node := &BrowserHistoryNode{
+		val:  url,
+		next: nil,
+		prev: this.curr,
+	}
+	this.curr.next = node
+	this.curr = node
+	this.tail = node
+}
+
+
+func (this *BrowserHistory) Back(steps int) string {
+	for i := 0; i < steps; i++ {
+		if this.curr.prev == nil {
+			return this.curr.val
+		}
+		this.curr = this.curr.prev
+	}
+	return this.curr.val
+}
+
+
+func (this *BrowserHistory) Forward(steps int) string {
+	for i := 0; i < steps; i++ {
+		if this.curr.next == nil {
+			return this.curr.val
+		}
+		this.curr = this.curr.next
+	}
+	return this.curr.val
+}
+
+
+/**
+ * Your BrowserHistory object will be instantiated and called as such:
+ * obj := Constructor(homepage);
+ * obj.Visit(url);
+ * param_2 := obj.Back(steps);
+ * param_3 := obj.Forward(steps);
+ */
