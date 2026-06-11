c80379b94d5179ed8281bdb8ea33bbecd7c76a90
gerardvega <101549941+gerardangelovega@users.noreply.github.com>
2026-06-11 10:26:32 +0800

    Add: remove-element - submission-2

---
+++ Data Structures & Algorithms/remove-element/submission-2.go
@@ -0,0 +1,19 @@
+func removeElement(nums []int, val int) int {
+	n := len(nums)
+	swap_index := n - 1
+
+	i := 0
+	for i < n {
+		if i > swap_index { 
+			break
+		}
+		if nums[i] == val {
+			nums[i], nums[swap_index] = nums[swap_index], nums[i]
+			swap_index--
+			continue
+		}
+		i++
+	}
+
+	return swap_index + 1
+}
