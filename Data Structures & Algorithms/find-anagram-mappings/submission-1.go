2a16eb799dc5e5f22c3ccca6913ec8dafedced74
gerardvega <101549941+gerardangelovega@users.noreply.github.com>
2026-06-17 23:45:16 +0800

    Add: find-anagram-mappings - submission-1

---
+++ Data Structures & Algorithms/find-anagram-mappings/submission-1.go
@@ -0,0 +1,18 @@
+func anagramMappings(nums1 []int, nums2 []int) []int {
+	m := make(map[int]int)
+	res := make([]int, len(nums1))
+
+	for _, num := range nums1 {
+		m[num] = -1
+	}
+
+	for i, num := range nums2 {
+		m[num] = i
+	}
+
+	for i, num := range nums1 {
+		res[i] = m[num]
+	}
+
+	return res
+}
