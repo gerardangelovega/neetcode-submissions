5223f669ae96c97f02927a13243d2d1367ad1702
gerardvega <101549941+gerardangelovega@users.noreply.github.com>
2026-06-21 20:14:08 +0800

    Add: sort-an-array - submission-0

---
+++ Data Structures & Algorithms/sort-an-array/submission-0.go
@@ -0,0 +1,24 @@
+func sortArray(nums []int) []int {
+	quickSort(nums, 0, len(nums) - 1)
+
+	return nums
+}
+
+func quickSort(arr []int, start, end int) {
+	if (end - start) + 1 <= 1 {
+		return
+	}
+
+	left := start
+	for i := start; i < end; i++ {
+		if arr[i] < arr[end] {
+			arr[i], arr[left] = arr[left], arr[i]
+			left++
+		}
+	}
+
+	arr[left], arr[end] = arr[end], arr[left]
+
+	quickSort(arr, start, left-1)
+	quickSort(arr, left+1, end)
+}
