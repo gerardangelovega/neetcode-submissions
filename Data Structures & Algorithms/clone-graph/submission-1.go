cecfb39e4121c4976c7483708e8cca9c065d7e1b
Gerard Angelo Vega <101549941+gerardangelovega@users.noreply.github.com>
2026-08-12 09:59:06 +0800

    Add: clone-graph - submission-1

---
+++ Data Structures & Algorithms/clone-graph/submission-1.go
@@ -0,0 +1,62 @@
+type Queue struct {
+	items []*Node
+}
+func NewQueue() Queue {
+	return Queue {
+		items: []*Node{},
+	}
+}
+func (q* Queue) Enqueue(val *Node) {
+	q.items = append(q.items, val)
+}
+func (q* Queue) Dequeue() *Node {
+	node := q.items[0]
+	q.items = q.items[1:]
+	return node
+}
+func (q* Queue) Length() int {
+	return len(q.items)
+}
+
+/**
+ * Definition for a Node.
+ * type Node struct {
+ *     Val int
+ *     Neighbors []*Node
+ * }
+ */
+func NewNode(val int) *Node {
+	return &Node {
+		Val: val,
+		Neighbors: []*Node{},
+	}
+}
+func (n *Node) AddNeighbor(node *Node) {
+	n.Neighbors = append(n.Neighbors, node)
+}
+
+func cloneGraph(node *Node) *Node {
+	if node == nil {
+		return nil
+	}
+	
+	copied := make(map[*Node]*Node)
+	queue := NewQueue()
+
+	copied[node] = NewNode(node.Val)
+	queue.Enqueue(node)
+
+	for queue.Length() != 0 {
+		curr := queue.Dequeue()
+
+		for _, nh := range curr.Neighbors {
+			if _, e := copied[nh]; !e {
+				copied[nh] = NewNode(nh.Val)
+				queue.Enqueue(nh)
+			}
+			copied[curr].AddNeighbor(copied[nh])
+		}
+	}
+
+	return copied[node]
+}
