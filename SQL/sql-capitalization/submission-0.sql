eedee5ca14101f4471598b4b192dd3e6a82cfaeb
Gerard Angelo Vega <101549941+gerardangelovega@users.noreply.github.com>
2026-08-13 11:48:26 +0800

    Add: sql-capitalization - submission-0

---
+++ SQL/sql-capitalization/submission-0.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS users (
+  name TEXT
+);
+
+
+-- Do not modify below this line --
+SELECT table_name, column_name, data_type
+FROM information_schema.columns
+WHERE table_name = 'users';
