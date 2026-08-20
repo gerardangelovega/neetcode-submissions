0b5d378f33344642787decb637427d65bc3e1e63
Gerard Angelo Vega <101549941+gerardangelovega@users.noreply.github.com>
2026-08-20 08:15:08 +0800

    Add: sql-rearrange-products-table - submission-0

---
+++ SQL/sql-rearrange-products-table/submission-0.sql
@@ -0,0 +1,15 @@
+-- Write your query below
+WITH product_cte AS (
+    SELECT product_id, 'store1' AS store, store1 AS price
+    FROM products
+    UNION ALL
+    SELECT product_id, 'store2' AS store, store2 AS price
+    FROM products
+    UNION ALL
+    SELECT product_id, 'store3' AS store, store3 AS price
+    FROM products
+)
+SELECT *
+FROM product_cte
+WHERE price IS NOT NULL
+;
