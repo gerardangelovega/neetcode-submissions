b7ff27f5c1acacfd78d4deb2fae76b1b809105a8
Gerard Angelo Vega <101549941+gerardangelovega@users.noreply.github.com>
2026-08-24 11:13:30 +0800

    Add: minimum-size-subarray-sum - submission-2

---
+++ Data Structures & Algorithms/minimum-size-subarray-sum/submission-2.go
@@ -0,0 +1,20 @@
+func minSubArrayLen(target int, nums []int) int {
+	l, r, n := 0, 0, len(nums)
+	res, sum := math.MaxInt, 0
+	_ = n
+
+	for l <= r {
+		if sum >= target {
+			res = min(res, r-l) 
+			sum = sum - nums[l]
+			l++
+		} else {
+			if r >= n { break }
+			sum = sum + nums[r]
+			r++
+		}
+	}
+
+	if res == math.MaxInt { return 0 }
+	return res
+}
