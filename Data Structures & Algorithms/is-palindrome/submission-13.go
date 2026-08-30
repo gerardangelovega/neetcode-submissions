6c7d1a513d523bdd8b70ddc4b2fa40492b44060f
Gerard Angelo Vega <101549941+gerardangelovega@users.noreply.github.com>
2026-08-30 20:51:00 +0800

    Add: is-palindrome - submission-13

---
+++ Data Structures & Algorithms/is-palindrome/submission-13.go
@@ -0,0 +1,10 @@
+func isPalindrome(s string) bool {
+    for l, r := 0, len(s)-1; l < r; l, r = l+1, r-1 {
+        for !(IsAlphaNumeric(s[l])) && l < r { l++ }
+        for !(IsAlphaNumeric(s[r])) && l < r { r-- }
+        if !IsEqual(s[l], s[r]) { return false }
+    }
+    return true
+}
+func IsAlphaNumeric(b byte) bool { return unicode.IsLetter(rune(b)) || unicode.IsNumber(rune(b)) }
+func IsEqual(a, b byte) bool { return unicode.ToLower(rune(a)) == unicode.ToLower(rune(b)) }
