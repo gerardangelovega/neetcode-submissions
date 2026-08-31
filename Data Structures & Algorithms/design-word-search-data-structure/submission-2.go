47ad2d6ebb35d4324273a87afca314fdaab2e607
Gerard Angelo Vega <101549941+gerardangelovega@users.noreply.github.com>
2026-08-31 12:02:47 +0800

    Add: design-word-search-data-structure - submission-2

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-2.go
@@ -0,0 +1,95 @@
+type Queue struct {
+	items []*WordDictionaryNode
+}
+
+func NewQueue() Queue {
+	return Queue{
+		items: make([]*WordDictionaryNode, 0),
+	}
+}
+
+func (q *Queue) Enqueue(val *WordDictionaryNode) {
+	q.items = append(q.items, val)
+}
+
+func (q *Queue) Dequeue() *WordDictionaryNode {
+	res := q.items[0]
+	q.items = q.items[1:]
+	return res
+}
+
+func (q *Queue) Length() int {
+	return len(q.items)
+}
+
+type WordDictionaryNode struct {
+	children map[rune]*WordDictionaryNode
+	word bool
+}
+
+func NewWordDictionaryNode() *WordDictionaryNode {
+	return &WordDictionaryNode{
+		children: make(map[rune]*WordDictionaryNode),
+		word: false,
+	}
+}
+
+type WordDictionary struct {
+	root *WordDictionaryNode 
+}
+
+func Constructor() WordDictionary {
+	return WordDictionary{
+		root: NewWordDictionaryNode(),
+	}
+}
+
+func (this *WordDictionary) AddWord(word string)  {
+    curr := this.root
+	for _, r := range word {
+		if _, e := curr.children[r]; !e {
+			curr.children[r] = NewWordDictionaryNode()
+		}
+		curr = curr.children[r]
+	}
+	curr.word = true
+}
+
+func (this *WordDictionary) Search(word string) bool {
+	return search(this.root, word)
+}
+
+func search(root *WordDictionaryNode, word string) bool {
+	r := rune(word[0])
+	q := NewQueue()
+	if r != '.' {
+		if _, e := root.children[r]; !e {
+			return false
+		}
+		if len(word) == 1 {
+			return root.children[r].word
+		}
+		q.Enqueue(root.children[r])
+	} else {
+		if len(word) == 1 {
+			for _, v := range root.children {
+				if v.word {
+					return true
+				}
+			}
+			return false
+		}
+		for _, v := range root.children {
+			q.Enqueue(v)
+		}
+	}
+
+	found := false
+
+	for q.Length() != 0 {
+		curr := q.Dequeue()
+		found = found || search(curr, word[1:])
+	}
+	
+	return found
+}
