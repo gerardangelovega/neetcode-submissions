602717539e5f55263a95bf06e38c8772b47d6614
Gerard Angelo Vega <101549941+gerardangelovega@users.noreply.github.com>
2026-09-01 11:19:45 +0800

    Add: insert-new-interval - submission-6

---
+++ Data Structures & Algorithms/insert-new-interval/submission-6.go
@@ -0,0 +1,27 @@
+func insert(intervals [][]int, newInterval []int) [][]int {
+	res := [][]int{}
+	n := newInterval
+	for _, interval := range intervals {
+		if n[0] != -1 && n[1] != -1 {
+			if n[1] < interval[0] { 
+				res = append(res, n)
+				res = append(res, interval)
+				n = []int{-1, -1}	
+				continue
+			}
+			if n[0] > interval[1] {
+				res = append(res, interval)
+				continue
+			}
+			if n[0] <= interval[1] { 
+				n[0], n[1] = min(interval[0], n[0]), max(interval[1], n[1])
+				continue
+			}
+		}
+		res = append(res, interval)
+	}
+	if n[0] != -1 && n[1] != -1 { 
+		res = append(res, n)
+	}
+	return res
+}
