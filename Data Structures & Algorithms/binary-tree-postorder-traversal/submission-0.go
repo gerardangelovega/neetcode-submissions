d9cafbc906583fb3b70b5f0a3fd216b1c4b99097
Gerard Angelo Vega <101549941+gerardangelovega@users.noreply.github.com>
2026-09-07 12:28:18 +0800

    Add: binary-tree-postorder-traversal - submission-0

---
+++ Data Structures & Algorithms/binary-tree-postorder-traversal/submission-0.go
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+func postorderTraversal(root *TreeNode) []int {
+	res := []int{}
+	postorder(root, &res)
+	return res
+}
+
+func postorder(root *TreeNode, res *[]int) {
+	if root == nil { return }
+	postorder(root.Left, res)
+	postorder(root.Right, res)
+	*res = append(*res, root.Val)
+}
