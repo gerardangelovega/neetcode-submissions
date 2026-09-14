cc6d123d37ea4f892722e356beeb3c576b337ca0
Gerard Angelo Vega <101549941+gerardangelovega@users.noreply.github.com>
2026-09-14 08:36:33 +0800

    Add: longest-palindromic-substring - submission-3

---
+++ Data Structures & Algorithms/longest-palindromic-substring/submission-3.go
@@ -0,0 +1,22 @@
+func longestPalindrome(s string) string {
+    res := ""
+	n := len(s)
+	for i := 0; i < n; i++ {
+		bfs(s, i, i, &res)	
+		bfs(s, i, i + 1, &res)	
+	}
+	return res
+}
+
+func bfs(s string, l, r int, res *string) {
+	for l > -1 && r < len(s) {
+		if s[l] != s[r] {
+			break
+		}
+		if len(s[l:r+1]) > len(*res) {
+			*res = s[l:r+1]
+		}
+		l--
+		r++	
+	}
+}
